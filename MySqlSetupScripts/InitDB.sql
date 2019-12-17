e2510674360b8645dae0fb7b3a17142309075531
Ken Nipper <ken@fngservices.com>
2019-12-17 10:54:50 -0500

    made changes to use sha password

--- MySqlSetupScripts/InitDB.sql
+++ MySqlSetupScripts/InitDB.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS ScoutDemo;
-CREATE USER IF NOT EXISTS 'scoutdemo'@'%' IDENTIFIED WITH mysql_native_password BY 'scoutdemo';
+CREATE USER IF NOT EXISTS 'scoutdemo'@'%' IDENTIFIED WITH caching_sha2_password BY 'scoutdemo';
 GRANT ALL ON ScoutDemo.* TO 'scoutdemo'@'%';
 
 DROP TABLE IF EXISTS `ScoutDemo`.`Person`;
