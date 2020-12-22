36f95aa689f372fa2b59ec417c1655bea9863344
Kenneth Nipper <knipper@quickrx.net>
2020-12-22 07:56:48 -0500

    filename changes

--- Site/dbconnection.php
+++ Site/dbconnection.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbconnection = mysqli_connect("mysql", "scoutdemo", "scoutdemo", "scoutdemo");
+$dbconnection = mysqli_connect("ScoutPHPDemo.MySQL", "scoutdemo", "scoutdemo", "scoutdemo");
 if ($dbconnection === false) {
     die("ERROR: could not connect. " . mysqli_connect_error());
 }
